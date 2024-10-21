09e9e50f67f0f83ff773802066cd0a1bb9e59916
Darren Shepherd <darren@acorn.io>
2024-10-21 14:29:04 -0700

    bug: remove type in tool filename

--- tools/package-tools.sh
+++ tools/package-tools.sh
@@ -12,14 +12,14 @@ for gomod in $(find otto8-tools -name go.mod); do
     (
         cd $(dirname $gomod)
         echo Building $PWD
-        go build -o bin/gptscript-go/tool .
+        go build -o bin/gptscript-go-tool .
     )
 done
 
 cd otto8-tools
 git clone --depth=1 https://github.com/gptscript-ai/workspace-provider
 cd workspace-provider
-go build -o bin/gptscript-go/tool .
+go build -o bin/gptscript-go-tool .
 rm -rf .git
 cd ..
 rm -rf .git
