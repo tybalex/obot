112cdc17938e03c3d957997af2e400d7e04bc5f4
tylerslaton <mtslaton1@gmail.com>
2024-10-24 09:48:15 -0400

    fix: use different key for tool reference getters
    This is a workaround to make the SWR caching work
    nicely with the getToolReferences function. The reason we do this
    is because otherwise the SWR cache will not be invalidated between
    the two function calls and thus break type safety since types in
    TypeScript are erased at runtime. as-map=true does not exist in
    the API.
    
    Signed-off-by: tylerslaton <mtslaton1@gmail.com>

--- ui/admin/app/lib/service/api/toolreferenceService.ts
+++ ui/admin/app/lib/service/api/toolreferenceService.ts
@@ -54,7 +54,13 @@ async function getToolReferencesCategoryMap(type?: ToolReferenceType) {
 }
 getToolReferencesCategoryMap.key = (type?: ToolReferenceType) =>
     ({
-        url: ApiRoutes.toolReferences.base({ type }).path,
+        // tylerslaton: This is a workaround to make the SWR caching work
+        // nicely with the getToolReferences function. The reason we do this
+        // is because otherwise the SWR cache will not be invalidated between
+        // the two function calls and thus break type safety since types in
+        // TypeScript are erased at runtime. as-map=true does not exist in
+        // the API.
+        url: ApiRoutes.toolReferences.base({ type }).path + "?as-map=true",
     }) as const;
 
 const getToolReferenceById = async (toolReferenceId: string) => {
