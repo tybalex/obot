60368564e602bc9c57195eb184944262168eeb64
Darren Shepherd <darren@acorn.io>
2024-11-13 12:05:50 -0700

    fix: add missing file

---
+++ apiclient/webhook.go
@@ -0,0 +1,41 @@
+package apiclient
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"sort"
+
+	"github.com/otto8-ai/otto8/apiclient/types"
+)
+
+func (c *Client) ListWebhooks(ctx context.Context) (result types.WebhookList, _ error) {
+	defer func() {
+		sort.Slice(result.Items, func(i, j int) bool {
+			return result.Items[i].Metadata.Created.Time.Before(result.Items[j].Metadata.Created.Time)
+		})
+	}()
+
+	_, resp, err := c.doRequest(ctx, http.MethodGet, "/webhooks", nil)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	_, err = toObject(resp, &result)
+	if err != nil {
+		return result, err
+	}
+
+	return result, nil
+}
+
+func (c *Client) DeleteWebhook(ctx context.Context, id string) error {
+	_, resp, err := c.doRequest(ctx, http.MethodDelete, fmt.Sprintf("/webhooks/"+id), nil)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return nil
+}
