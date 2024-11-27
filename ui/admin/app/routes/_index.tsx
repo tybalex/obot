79fb5b5cda58ab3c31dd9adc37f98a3939dde3df
Ryan Hopper-Lowe <46546486+ryanhopperlowe@users.noreply.github.com>
2024-11-27 10:37:09 -0600

    fix: Prevent browser tab title from being set to "New Remix App" (#708)
    Signed-off-by: Ryan Hopper-Lowe <ryan@acorn.io>

--- ui/admin/app/routes/_index.tsx
+++ ui/admin/app/routes/_index.tsx
@@ -1,12 +1,5 @@
-import { type MetaFunction, redirect, useLoaderData } from "@remix-run/react";
+import { redirect, useLoaderData } from "@remix-run/react";
 import { $path } from "remix-routes";
-
-export const meta: MetaFunction = () => {
-    return [
-        { title: "New Remix App" },
-        { name: "description", content: "Welcome to Remix!" },
-    ];
-};
 
 export const clientLoader = async () => {
     throw redirect($path("/agents"));
