b13c3a8be78cc56ed9edfff839b3cd8d8c9798d8
Ryan Hopper-Lowe <46546486+ryanhopperlowe@users.noreply.github.com>
2024-12-05 14:13:58 -0600

    enhance: agent name generation in admin ui (#784)
    Signed-off-by: Ryan Hopper-Lowe <ryan@acorn.io>

--- ui/admin/app/lib/service/nameGenerator.ts
+++ ui/admin/app/lib/service/nameGenerator.ts
@@ -3,8 +3,28 @@ import { faker } from "@faker-js/faker";
 const uppercaseFirst = (word: string) =>
     word.charAt(0).toUpperCase() + word.slice(1);
 
+const foodNouns = [
+    faker.food.dish,
+    faker.food.fruit,
+    faker.food.ingredient,
+    faker.food.meat,
+    faker.food.spice,
+    faker.food.vegetable,
+];
+
+const nounOptions = [
+    faker.animal.type,
+    faker.commerce.product,
+    ...faker.helpers.arrayElements(foodNouns, 2),
+    faker.hacker.noun,
+    faker.person.zodiacSign,
+    faker.person.jobDescriptor,
+    faker.vehicle.type,
+];
+
 export function generateRandomName(): string {
-    return [faker.word.adjective(), faker.word.noun()]
+    return `${faker.word.adjective()} ${faker.helpers.arrayElement(nounOptions)()}`
+        .split(" ")
         .map(uppercaseFirst)
         .join(" ");
 }
