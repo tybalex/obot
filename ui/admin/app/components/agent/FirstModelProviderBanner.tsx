caf47d71b8d7ba9c0f3a6dfde81e9f65e6ba5345
Ryan Hopper-Lowe <46546486+ryanhopperlowe@users.noreply.github.com>
2024-12-06 12:58:29 -0600

    enhance: improve agent banner (#799)

--- ui/admin/app/components/agent/FirstModelProviderBanner.tsx
+++ ui/admin/app/components/agent/FirstModelProviderBanner.tsx
@@ -8,8 +8,8 @@ import { Button } from "~/components/ui/button";
 export function FirstModelProviderBanner() {
     return (
         <div className="w-full">
-            <div className="flex justify-center w-full">
-                <div className="flex flex-row p-4 min-h-36 justify-end items-center w-[calc(100%-4rem)] rounded-sm mx-8 mt-4 bg-secondary relative overflow-hidden gap-4 max-w-screen-md">
+            <div className="flex justify-center mx-8 mt-4 py-4 bg-secondary overflow-hidden rounded-xl">
+                <div className="flex flex-row min-h-36 items-center justify-between w-[calc(100%-4rem)] rounded-sm relative gap-4 max-w-screen-md">
                     <OttoLogo
                         hideText
                         classNames={{
@@ -17,13 +17,13 @@ export function FirstModelProviderBanner() {
                             image: "h-80 w-80",
                         }}
                     />
-                    <div className="flex flex-col pl-48">
+                    <div className="flex flex-col ml-64">
                         <TypographyH3 className="mb-0.5">
                             Ready to create your first Agent?
                         </TypographyH3>
                         <TypographyP className="text-sm font-light mb-2">
                             You&apos;re almost there! To start creating or using{" "}
-                            agents, you&apos;ll need access to a LLM (Large
+                            agents, you&apos;ll need access to an LLM (Large
                             Language Model) <b>Model Provider</b>. Luckily, we
                             support a variety of providers to help get you
                             started.
