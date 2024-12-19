e394134af8ab47369c328d5d86a0522a00f4b045
Ivy <ivy@acorn.io>
2024-12-19 11:46:00 -0500

    fix: click edit button opens workflow (#977)

--- ui/admin/app/routes/_auth.workflows._index.tsx
+++ ui/admin/app/routes/_auth.workflows._index.tsx
@@ -66,6 +66,14 @@ export default function Workflows() {
         );
     }, [getThreads.data, getWorkflows.data]);
 
+    const navigateToWorkflow = (workflow: Workflow) => {
+        navigate(
+            $path("/workflows/:workflow", {
+                workflow: workflow.id,
+            })
+        );
+    };
+
     return (
         <div>
             <div className="h-full p-8 flex flex-col gap-4">
@@ -83,13 +91,7 @@ export default function Workflows() {
                         disableClickPropagation={(cell) =>
                             cell.id.includes("action")
                         }
-                        onRowClick={(row) => {
-                            navigate(
-                                $path("/workflows/:workflow", {
-                                    workflow: row.id,
-                                })
-                            );
-                        }}
+                        onRowClick={navigateToWorkflow}
                     />
                 </div>
             </div>
@@ -143,7 +145,13 @@ export default function Workflows() {
 
                         <Tooltip>
                             <TooltipTrigger asChild>
-                                <Button size="icon" variant="ghost">
+                                <Button
+                                    size="icon"
+                                    variant="ghost"
+                                    onClick={() =>
+                                        navigateToWorkflow(row.original)
+                                    }
+                                >
                                     <PenSquareIcon />
                                 </Button>
                             </TooltipTrigger>
