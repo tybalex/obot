c4b520611cdaaee2cf172f27284ded4d74b31194
Nick Hale <4175918+njhale@users.noreply.github.com>
2024-12-19 11:16:48 -0500

    Revert "chore: hide atlassian oauth config from admins"
    This reverts commit 42adf476ec792437d00c6b94c4e7be0676171d6d.

--- ui/admin/app/lib/model/oauthApps/index.ts
+++ ui/admin/app/lib/model/oauthApps/index.ts
@@ -2,6 +2,7 @@ import {
     OAuthAppSpec,
     OAuthProvider,
 } from "~/lib/model/oauthApps/oauth-helpers";
+import { AtlassianOAuthApp } from "~/lib/model/oauthApps/providers/atlassian";
 import { GitHubOAuthApp } from "~/lib/model/oauthApps/providers/github";
 import { GoogleOAuthApp } from "~/lib/model/oauthApps/providers/google";
 import { Microsoft365OAuthApp } from "~/lib/model/oauthApps/providers/microsoft365";
@@ -11,6 +12,7 @@ import { SlackOAuthApp } from "~/lib/model/oauthApps/providers/slack";
 import { EntityMeta } from "~/lib/model/primitives";
 
 export const OAuthAppSpecMap = {
+    [OAuthProvider.Atlassian]: AtlassianOAuthApp,
     [OAuthProvider.GitHub]: GitHubOAuthApp,
     [OAuthProvider.Google]: GoogleOAuthApp,
     [OAuthProvider.Microsoft365]: Microsoft365OAuthApp,
