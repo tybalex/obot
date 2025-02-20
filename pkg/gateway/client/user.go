d4f06198b157bb7a5bdb62b47adbe055003a425b
Grant Linville <grant@acorn.io>
2025-02-20 09:25:27 -0500

    fix: profile images: load new profile image if user switched auth providers (#1785)
    Signed-off-by: Grant Linville <grant@acorn.io>

--- pkg/gateway/client/user.go
+++ pkg/gateway/client/user.go
@@ -125,8 +125,8 @@ func (c *Client) UpdateProfileIconIfNeeded(ctx context.Context, user *types.User
 		return err
 	}
 
-	if time.Until(identity.IconLastChecked) > -7*24*time.Hour {
-		// Icon was checked less than 7 days ago.
+	if user.IconURL == identity.IconURL && time.Until(identity.IconLastChecked) > -7*24*time.Hour {
+		// Icon was checked less than 7 days ago and the user is still using the same auth provider.
 		return nil
 	}
 
