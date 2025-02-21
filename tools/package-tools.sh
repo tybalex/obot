ff30dffbe82b6ca80e8d1df2ce64f46f085a9d6a
Grant Linville <grant@acorn.io>
2025-02-21 15:11:57 -0500

    chore: use our fork of k8s-cloudkms-plugin (#1822)
    Signed-off-by: Grant Linville <grant@acorn.io>

--- tools/package-tools.sh
+++ tools/package-tools.sh
@@ -132,12 +132,12 @@ VERSIONS
 cd ..
 
 if [ ! -e k8s-cloudkms-plugin ]; then
-	git clone --depth=1 https://github.com/GoogleCloudPlatform/k8s-cloudkms-plugin
+	git clone --depth=1 https://github.com/obot-platform/k8s-cloudkms-plugin
 fi
 cd k8s-cloudkms-plugin
 go build -ldflags "-s -w -extldflags 'static'" -installsuffix cgo -tags netgo -o "${BIN_DIR}/gcp-encryption-provider" cmd/k8s-cloudkms-plugin/main.go
 OBOT_SERVER_VERSIONS="$(cat <<VERSIONS
-"github.com/GoogleCloudPlatform/k8s-cloudkms-plugin": "$(git rev-parse --short HEAD)"
+"github.com/obot-platform/k8s-cloudkms-plugin": "$(git rev-parse --short HEAD)"
 ${OBOT_SERVER_VERSIONS}
 VERSIONS
 )"
