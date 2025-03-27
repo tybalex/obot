8ebf5ae0da8754e68836978292e2f22736bb0918
Ivy <ivy@acorn.io>
2025-03-27 19:36:35 -0400

    fix: line clamp on error notifications (#2310)

--- ui/user/src/lib/components/Notifications.svelte
+++ ui/user/src/lib/components/Notifications.svelte
@@ -23,16 +23,18 @@
 <div bind:this={div} class="absolute right-0 bottom-0 z-50 hidden flex-col gap-2 pr-5 pb-5">
 	{#each errors.items as error, i}
 		<div
-			class="relative flex max-w-sm items-center gap-2 rounded-3xl bg-gray-50 p-5 dark:bg-gray-950"
+			class="relative flex max-w-sm items-center gap-2 rounded-xl bg-gray-50 p-5 pr-12 dark:bg-gray-950"
 		>
 			<div>
 				<CircleX class="h-5 w-5" />
 			</div>
-			<div class="pr-5 text-sm font-normal">{error.message}</div>
+			<div class="line-clamp-3 pr-5 text-sm font-normal break-all">
+				{error.message}
+			</div>
 			<button
 				type="button"
 				onclick={() => errors.items.splice(i, 1)}
-				class="absolute top-0 right-0 p-5"
+				class="absolute top-0 right-0 p-4"
 			>
 				<X class="h-5 w-5" />
 			</button>
