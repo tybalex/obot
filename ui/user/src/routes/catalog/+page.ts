2ee3f145148bd5bfc3ab56a7611cd99e60e34feb
Darren Shepherd <darren@acorn.io>
2025-04-10 10:07:38 -0700

    bug: more bad hacks around ssr (#2467)

--- ui/user/src/routes/catalog/+page.ts
+++ ui/user/src/routes/catalog/+page.ts
@@ -1,16 +1,8 @@
-import { browser, building } from '$app/environment';
+import { browser } from '$app/environment';
 import { ChatService } from '$lib/services';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch }) => {
-	// to be perfectly honest, I have no idea why this is needed, you'd assume the catch below would be enough
-	if (building) {
-		return {
-			shares: [],
-			tools: []
-		};
-	}
-
 	try {
 		const shares = ChatService.listProjectShares({ fetch });
 		const tools = ChatService.listAllTools({ fetch });
