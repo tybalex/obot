84761ddd16e8c514230eec3ae1ab7e51f84c7477
Donnie Adams <donnie@acorn.io>
2025-06-20 12:31:52 -0400

    fix: detect _exec nanobot command for running stdio servers
    Signed-off-by: Donnie Adams <donnie@acorn.io>

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	_ "time/tzdata"
 
 	"github.com/gptscript-ai/cmd"
 	"github.com/gptscript-ai/gptscript/pkg/embedded"
+	"github.com/nanobot-ai/nanobot/pkg/supervise"
 	"github.com/obot-platform/obot/pkg/cli"
 )
 
@@ -14,6 +16,13 @@ func main() {
 		if embedded.Run(embedded.Options{}) {
 			return
 		}
+	}
+	if len(os.Args) > 1 && os.Args[1] == "_exec" {
+		if err := supervise.Daemon(); err != nil {
+			fmt.Printf("failed to run nanobot daemon: %v\n", err)
+			os.Exit(1)
+		}
+		os.Exit(0)
 	}
 	// Don't shutdown on SIGTERM, only on SIGINT. SIGTERM is handled by the controller leader election
 	cmd.ShutdownSignals = []os.Signal{os.Interrupt}
